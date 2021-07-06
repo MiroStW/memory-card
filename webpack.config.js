1c66d8ccf43cb33efb22639b0fd75c3a6af862fe
Miro <miro@miro-wilms.de>
2021-07-06 16:57:45 +0200

    switched to /docs output path for ghpages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: ["./src/index.js", "./src/style.css"],
   output: {
     filename: "[name].js", // filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, "dist/"),
+    path: path.resolve(__dirname, "docs/"),
     clean: true,
   },
   module: {
