4e73a2c7062dc355326ffeba82ce1e285ebd4a5a
Miro <miro@miro-wilms.de>
2021-07-23 16:46:48 +0200

    refactored eventlistener

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,28 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 const Result = (props) => {
-  useEffect(() => {
-    document.querySelector("#result").addEventListener("click", () => {
-      props.setResult(null);
-    });
-    return document
-      .querySelector("#result")
-      .removeEventListener("click", () => {
-        props.setResult(null);
-      });
-  }, [props]);
-
   if (props.result === "won") {
     return (
-      <div id="result" className="won">
+      <div id="result" className="won" onClick={() => props.setResult(null)}>
         <div>You won :-)</div>
       </div>
     );
   }
 
   return (
-    <div id="result" className="lost">
+    <div id="result" className="lost" onClick={() => props.setResult(null)}>
       <div>You lost :-(</div>
     </div>
   );
