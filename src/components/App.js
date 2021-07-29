624f8d773c2da7311ec886f99467067a586a3107
Miro <miro@miro-wilms.de>
2021-07-29 14:20:46 +0200

    extracted related elements to Board component

--- src/components/App.js
+++ src/components/App.js
@@ -1,68 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Board from "./Board";
-import Result from "./Result";
 import DifficultySwitch from "./DifficultySwitch";
-import loadCards from "./loadCards";
 
 const App = () => {
   const [totalCards, setTotalCards] = useState(8);
-  const [cards, setCards] = useState({});
   const [clickedCards, setClickedCards] = useState([]);
   const [bestScore, setBestScore] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
-  const [gameResult, setGameResult] = useState(null);
-
-  // Game logic component
-  const handleCardClick = (id) => {
-    if (clickedCards.includes(id)) {
-      console.log("card already clicked");
-      if (clickedCards.length > bestScore) setBestScore(clickedCards.length);
-      setGameResult("lost");
-      setClickedCards([]);
-    } else {
-      console.log("new card clicked");
-      setClickedCards([...clickedCards, id]);
-    }
-  };
-
-  // if no of total cards is changed (e.g. game started or difficulty changed)
-  useEffect(() => {
-    setCards({});
-    (async () => {
-      setIsLoading(true);
-      setCards(await loadCards(totalCards));
-      setIsLoading(false);
-    })();
-    setClickedCards([]);
-    // console.log(`# total cards ${totalCards}`);
-  }, [totalCards]);
-
-  useEffect(() => {
-    // if all cards clicked // WON
-    if (
-      clickedCards.length === Object.keys(cards).length &&
-      clickedCards.length > 0
-    ) {
-      setGameResult("won");
-      setClickedCards([]);
-      (async () => {
-        setIsLoading(true);
-        setCards(await loadCards(totalCards));
-        setIsLoading(false);
-      })();
-      console.log("WON!!!");
-    }
-
-    if (totalCards === Object.keys(cards).length)
-      console.log(
-        `# clickedCards: ${clickedCards.length} / ${Object.keys(cards).length}`
-      );
-  }, [cards, clickedCards.length, totalCards]);
-
-  // useEffect(() => {
-  //   console.log(`shuffled cards:`);
-  //   console.log(cards);
-  // }, [cards]);
 
   return (
     <>
@@ -80,27 +23,19 @@ const App = () => {
           totalCards={totalCards}
           setTotalCards={setTotalCards}
         />
-        {/* add select to increase difficulty */}
         {/* add new cards btn */}
       </div>
       <div id="cards">
-        {gameResult && <Result result={gameResult} setResult={setGameResult} />}
-        {isLoading ? (
-          <div>loading...</div>
-        ) : (
-          <Board cards={cards} handleCardClick={() => handleCardClick()} />
-        )}
+        <Board
+          totalCards={totalCards}
+          clickedCards={clickedCards}
+          setClickedCards={setClickedCards}
+          bestScore={bestScore}
+          setBestScore={setBestScore}
+        />
       </div>
     </>
   );
 };
 
 export default App;
-
-// project outline:
-// Scoreboard: counts the current score, and a “Best Score”, which shows the best
-//   score you achieved thus far
-// Cards: couple of cards that display images and possibly informational text.
-// Randomise order: function that displays the cards in a random order anytime a
-//   user clicks one (invoke on mount)
-//
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,19 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import Result from "./Result";
+import loadCards from "./loadCards";
 import Card from "./Card";
 
 // Cards component
-const Board = (props) =>
-  Object.keys(props.cards)
-    .map((card) => ({ sort: Math.random(), value: card }))
-    .sort((a, b) => a.sort - b.sort)
-    .map((card) => card.value)
-    .map((card) => (
-      <Card
-        key={card}
-        id={card}
-        cardObject={props.cards[card]}
-        handleCardClick={props.handleCardClick}
-      />
-    ));
+const Board = ({
+  totalCards,
+  clickedCards,
+  setClickedCards,
+  bestScore,
+  setBestScore,
+}) => {
+  const [cards, setCards] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [gameResult, setGameResult] = useState(null);
+
+  // Game logic component
+  const handleCardClick = (id) => {
+    if (clickedCards.includes(id)) {
+      console.log("card already clicked");
+      if (clickedCards.length > bestScore) setBestScore(clickedCards.length);
+      setGameResult("lost");
+      setClickedCards([]);
+    } else {
+      console.log("new card clicked");
+      setClickedCards([...clickedCards, id]);
+    }
+  };
+
+  // if no of total cards is changed (e.g. game started or difficulty changed)
+  useEffect(() => {
+    setCards({});
+    (async () => {
+      setIsLoading(true);
+      setCards(await loadCards(totalCards));
+      setIsLoading(false);
+    })();
+    setClickedCards([]);
+    // console.log(`# total cards ${totalCards}`);
+  }, [totalCards]);
+
+  useEffect(() => {
+    // if all cards clicked // WON
+    if (
+      clickedCards.length === Object.keys(cards).length &&
+      clickedCards.length > 0
+    ) {
+      setGameResult("won");
+      setClickedCards([]);
+      (async () => {
+        setIsLoading(true);
+        setCards(await loadCards(totalCards));
+        setIsLoading(false);
+      })();
+      console.log("WON!!!");
+    }
+
+    if (totalCards === Object.keys(cards).length)
+      console.log(
+        `# clickedCards: ${clickedCards.length} / ${Object.keys(cards).length}`
+      );
+  }, [cards, clickedCards.length, totalCards]);
+
+  return (
+    <>
+      {gameResult && <Result result={gameResult} setResult={setGameResult} />}
+      {isLoading ? (
+        <div>loading...</div>
+      ) : (
+        Object.keys(cards)
+          .map((card) => ({ sort: Math.random(), value: card }))
+          .sort((a, b) => a.sort - b.sort)
+          .map((card) => card.value)
+          .map((card) => (
+            <Card
+              key={card}
+              id={card}
+              cardObject={cards[card]}
+              handleCardClick={handleCardClick}
+            />
+          ))
+      )}
+    </>
+  );
+};
 
 export default Board;
+
+Board.propTypes = {
+  totalCards: PropTypes.number,
+  clickedCards: PropTypes.array,
+  setClickedCards: PropTypes.func,
+  bestScore: PropTypes.number,
+  setBestScore: PropTypes.func,
+};
