19c6cbeebc7904555b599d7051b92d35142ea781
Miro <miro@miro-wilms.de>
2021-10-15 14:45:44 +0200

    added jest linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,28 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
   extends: [
     "airbnb-base",
     "prettier",
     "eslint:recommended",
     "plugin:react/recommended",
+    "plugin:jest/recommended",
   ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react-hooks"],
+  plugins: ["react-hooks", "jest"],
   rules: {
     "no-unused-expressions": ["error", { allowTernary: true }],
     "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
     "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error",
   },
 };
