da09846831655041cd381540fe30d9a9b3ee9cfc
Miro <miro@miro-wilms.de>
2022-07-20 22:00:30 +0200

    fixed ts eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   env: {
     browser: true,
     es2021: true,
@@ -8,6 +15,10 @@ module.exports = {
     "airbnb-base",
     "prettier",
     "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:jest/recommended",
   ],
@@ -18,8 +29,14 @@ module.exports = {
   plugins: ["react-hooks", "jest"],
   rules: {
     "no-unused-expressions": ["error", { allowTernary: true }],
+    "arrow-body-style": "off",
+    "no-else-return": "off",
+    "import/extensions": "off",
+    "import/prefer-default-export": "off",
     "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
     "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
     "jest/no-disabled-tests": "warn",
     "jest/no-focused-tests": "error",
     "jest/no-identical-title": "error",
